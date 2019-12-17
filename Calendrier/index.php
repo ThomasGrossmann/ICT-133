7520620773a459957330b0e96af6dae39d9fc4a5
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2019-12-17 22:15:24 +0100

    FIN

--- Calendrier/index.php
+++ Calendrier/index.php
@@ -12,75 +12,87 @@
  * Auteur : Thomas Grossmann
  * Date : 05.12.2019
  */
-$monthQS = $_GET['month'];
-$yearQS = $_GET['year'];
-$dateTab = array("Mo", "Tu", "We", "Th", "Fr", "Sa", "Su");
-$ts = time();
-$date = date("n", $ts);
-$timestamp = $date % 7;
-$prev = $timestamp + $date;
-$next = $timestamp - $date +8;
-
-switch ($monthQS){
-    case 1:
-        echo "<div class='month'><ul><li>Janvier<br>$yearQS</li></ul></div>";
-        break;
-    case 2:
-        echo "<div class='month'><ul><li>Février<br>$yearQS</li></ul></div>";
-        break;
-    case 3:
-        echo "<div class='month'><ul><li>Mars<br>$yearQS</li></ul></div>";
-        break;
-    case 4:
-        echo "<div class='month'><ul><li>Avril<br>$yearQS</li></ul></div>";
-        break;
-    case 5:
-        echo "<div class='month'><ul><li>Mai<br>$yearQS</li></ul></div>";
-        break;
-    case 6:
-        echo "<div class='month'><ul><li>Juin<br>$yearQS</li></ul></div>";
-        break;
-    case 7:
-        echo "<div class='month'><ul><li>Juillet<br>$yearQS</li></ul></div>";
-        break;
-    case 8:
-        echo "<div class='month'><ul><li>Août<br>$yearQS</li></ul></div>";
-        break;
-    case 9:
-        echo "<div class='month'><ul><li>Septembre<br>$yearQS</li></ul></div>";
-        break;
-    case 10:
-        echo "<div class='month'><ul><li>Octobre<br>$yearQS</li></ul></div>";
-        break;
-    case 11:
-        echo "<div class='month'><ul><li>Novembre<br>$yearQS</li></ul></div>";
-        break;
-    case 12:
-        echo "<div class='month'><ul><li>Décembre<br>$yearQS</li></ul></div>";
-        break;
-}
-echo "<ul class='weekdays'>";
-for ($i = 0; $i < 7; $i++){
-    echo "<li>$dateTab[$i]</li>";
-}
-echo "</ul>";
-echo "<ul class='days'>";
-for ($i = 0; $i < $timestamp+1; $i++){
-    echo "<li><span class='prevnext'>$prev</li>";
-    $prev++;
-}
-for ($a = 1; $a <= 31; $a++){
-    echo "<li>";
-    if ($date == $a-1){
-        echo "<span class='active'>";
+function PageCalendrier()
+{
+    $monthQS = $_GET["month"];
+    $yearQS = $_GET["year"];
+    $dateTab = array("Mo", "Tu", "We", "Th", "Fr", "Sa", "Su");
+    $date = strtotime("$yearQS-$monthQS-1");
+    $FirstDayCurrentMonth = date("N", strtotime("first day of this month", $date));
+    $LastDayCurrentMonth = date("N", strtotime("last day of this month", $date));
+    $prev = date("j", strtotime("last day of last month", $date));
+    function getDaysBefore($prev, $FirstDayCurrentMonth)
+    {
+        for ($i = $prev - $FirstDayCurrentMonth + 2; $i <= $prev; $i++) {
+            echo "<li><span class='prevnext'>$i</li>";
+        }
     }
-    echo "$a</li>";
+    function getDaysAfter($LastDayCurrentMonth)
+    {
+        for ($i = 1; $i <= 7 - $LastDayCurrentMonth; $i++) {
+            echo "<li><span class='prevnext'>$i</li>";
+        }
+    }
+    function Calendrier($date, $yearQS, $monthQS)
+    {
+        for ($a = 1; $a <= date("j", strtotime("last day of this month", $date)); $a++) {
+            echo "<li>";
+            if ($a == date("j", strtotime("now")) && $yearQS == date("Y",strtotime("now")) && $monthQS == date("m",strtotime("now"))) {
+                echo "<span class='active'>";
+            }
+            echo "$a</li>";
+        }
+    }
+    switch ($monthQS) {
+        case 1:
+            echo "<div class='month'><ul><li>Janvier<br>$yearQS</li></ul></div>";
+            break;
+        case 2:
+            echo "<div class='month'><ul><li>Février<br>$yearQS</li></ul></div>";
+            break;
+        case 3:
+            echo "<div class='month'><ul><li>Mars<br>$yearQS</li></ul></div>";
+            break;
+        case 4:
+            echo "<div class='month'><ul><li>Avril<br>$yearQS</li></ul></div>";
+            break;
+        case 5:
+            echo "<div class='month'><ul><li>Mai<br>$yearQS</li></ul></div>";
+            break;
+        case 6:
+            echo "<div class='month'><ul><li>Juin<br>$yearQS</li></ul></div>";
+            break;
+        case 7:
+            echo "<div class='month'><ul><li>Juillet<br>$yearQS</li></ul></div>";
+            break;
+        case 8:
+            echo "<div class='month'><ul><li>Août<br>$yearQS</li></ul></div>";
+            break;
+        case 9:
+            echo "<div class='month'><ul><li>Septembre<br>$yearQS</li></ul></div>";
+            break;
+        case 10:
+            echo "<div class='month'><ul><li>Octobre<br>$yearQS</li></ul></div>";
+            break;
+        case 11:
+            echo "<div class='month'><ul><li>Novembre<br>$yearQS</li></ul></div>";
+            break;
+        case 12:
+            echo "<div class='month'><ul><li>Décembre<br>$yearQS</li></ul></div>";
+            break;
+    }
+    echo "<ul class='weekdays'>";
+    for ($i = 0; $i < 7; $i++) {
+        echo "<li>$dateTab[$i]</li>";
+    }
+    echo "</ul>";
+    echo "<ul class='days'>";
+    echo getDaysBefore($prev, $FirstDayCurrentMonth);
+    echo Calendrier($date, $yearQS, $monthQS);
+    echo getDaysAfter($LastDayCurrentMonth);
+    echo "</ul>";
 }
-for ($i = 0; $i < $timestamp; $i++) {
-    echo "<li><span class='prevnext'>$next</li>";
-    $next++;
-}
-echo "</ul>";
+echo PageCalendrier();
 ?>
 </body>
 </html>
